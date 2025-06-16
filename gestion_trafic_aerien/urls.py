ac5b54b9ac3dc64d8aaa77cba63976eda4ee3664
Wipeey <wipeey@offline.com>
2025-06-16 23:16:28 +0200

    Adding all tables info html list pages

--- gestion_trafic_aerien/urls.py
+++ gestion_trafic_aerien/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('aeroport/liste', views.aeroport_list, name="aeroport_list"),
-    path('avion/liste', views.avion_list, name="avion_list")
+    path('avion/liste', views.avion_list, name="avion_list"),
+    path('compagnie/liste', views.compagnie_list, name="compagnie_list"),
+    path('piste/liste', views.piste_list, name="piste_list"),
+    path('typeavion/liste', views.type_avion_list, name="type_avion_list"),
+    path('vol/liste', views.vol_list, name="vol_list")
 ]
--- gestion_trafic_aerien/views.py
+++ gestion_trafic_aerien/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Aeroport, Avion
+from .models import Aeroport, Avion, Compagnie, Piste, TypeAvion, Vol
 
 def index(request):
     return render(request, 'trafic_aerien/index.html')
@@ -8,7 +8,28 @@ def aeroport_list(request):
     aeroports = Aeroport.objects.all()
     return render(request, 'trafic_aerien/aeroport/list.html', {'aeroports': aeroports})
 
-
 def avion_list(request):
     avions = Avion.objects.all()
     return render(request, 'trafic_aerien/avion/list.html', {'avions': avions})
+
+def compagnie_list(request):
+    compagnies = Compagnie.objects.all()
+    return render(request, 'trafic_aerien/compagnie/list.html', {'compagnies': compagnies})
+
+def piste_list(request):
+    pistes = Piste.objects.all()
+    return render(request, 'trafic_aerien/piste/list.html', {'pistes': pistes})
+
+def type_avion_list(request):
+    types_avion = TypeAvion.objects.all()
+    return render(request, 'trafic_aerien/typeavion/list.html', {'types_avion': types_avion})
+
+def vol_list(request):
+    vols = (
+        Vol.objects
+           .select_related('avion', 'aeroport_depart', 'aeroport_arrivee')
+           .all()
+    )
+    return render(request, 'trafic_aerien/vol/list.html', {
+        'vols': vols,
+    })
