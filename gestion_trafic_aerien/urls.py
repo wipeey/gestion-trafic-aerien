f80736d4fde13308194041115351918cacf57832
Kenzy Laboyrie <kenzy.laboyrie@uha.fr>
2025-06-16 14:52:34 +0200

    Adding avions list page and handling urls/views

--- gestion_trafic_aerien/urls.py
+++ gestion_trafic_aerien/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('aeroport/liste', views.aeroport_list, name="aeroport_list")
+    path('aeroport/liste', views.aeroport_list, name="aeroport_list"),
+    path('avion/liste', views.avion_list, name="avion_list")
 ]
--- gestion_trafic_aerien/views.py
+++ gestion_trafic_aerien/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Aeroport
+from .models import Aeroport, Avion
 
 def index(request):
     return render(request, 'trafic_aerien/index.html')
@@ -7,3 +7,8 @@ def index(request):
 def aeroport_list(request):
     aeroports = Aeroport.objects.all()
     return render(request, 'trafic_aerien/aeroport/list.html', {'aeroports': aeroports})
+
+
+def avion_list(request):
+    avions = Avion.objects.all()
+    return render(request, 'trafic_aerien/avion/list.html', {'avions': avions})
