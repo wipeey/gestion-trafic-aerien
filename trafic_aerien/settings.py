5ef75e57d3fe4f42141a45649d97948e3743df88
Wipeey <wipeey@offline.com>
2025-06-17 23:44:54 +0200

    Adding requirements

--- trafic_aerien/settings.py
+++ trafic_aerien/settings.py
@@ -124,7 +124,7 @@ TIME_ZONE = 'Europe/Paris'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
